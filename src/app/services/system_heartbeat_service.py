9ee5ea40025b22b66d0e69342845965ebc2714fb
Daniel Collier <daniel.collier@luizalabs.com>
2026-05-16 15:51:23 -0300

    feat: prometheus service

--- src/app/services/system_heartbeat_service.py
+++ src/app/services/system_heartbeat_service.py
@@ -15,6 +15,7 @@ import uuid
 from datetime import datetime
 from pathlib import Path
 
+from ..services.prometheus_service import PrometheusService
 from ..settings import SystemMetrics, settings
 
 logger = logging.getLogger(__name__)
@@ -59,6 +60,7 @@ class SystemHeartbeatService:
 
         try:
             cpu, ram, temp, disk, disk_attached = SystemMetrics.get_stats()
+            PrometheusService().update_system(cpu, temp, ram=ram, disk=disk, disk_attached=disk_attached)
             # id, timestamp, label, confidence, rms, dbspl, flux, cpu, ram, temp, rom, rom attached
             row = [
                 f"{uuid.uuid4()}",
